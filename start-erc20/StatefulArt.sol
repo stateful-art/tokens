f7746de3d6f210b52804ca627b23fdddb9514ad2
Abbas Tolgay Yilmaz <abbas.tolgay.yilmaz@protonmail.com>
2022-04-01 14:09:07 +0200

    add genre tokens, with snapshots and supply with 1 Quadrillion supply

--- start-erc20/StatefulArt.sol
+++ start-erc20/StatefulArt.sol
@@ -3,18 +3,15 @@ pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20SnapshotUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/draft-ERC20PermitUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20FlashMintUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 /// @custom:security-contact contact@stateful.art
-contract StatefulArt is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, ERC20SnapshotUpgradeable, AccessControlUpgradeable, PausableUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, ERC20FlashMintUpgradeable, UUPSUpgradeable {
-    bytes32 public constant SNAPSHOT_ROLE = keccak256("SNAPSHOT_ROLE");
+contract StatefulArt is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, PausableUpgradeable, AccessControlUpgradeable, ERC20PermitUpgradeable, ERC20VotesUpgradeable, UUPSUpgradeable {
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
@@ -25,23 +22,16 @@ contract StatefulArt is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeabl
     function initialize() initializer public {
         __ERC20_init("StatefulArt", "START");
         __ERC20Burnable_init();
-        __ERC20Snapshot_init();
-        __AccessControl_init();
         __Pausable_init();
+        __AccessControl_init();
         __ERC20Permit_init("StatefulArt");
-        __ERC20FlashMint_init();
         __UUPSUpgradeable_init();
 
-        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _setupRole(SNAPSHOT_ROLE, msg.sender);
-        _setupRole(PAUSER_ROLE, msg.sender);
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
         _mint(msg.sender, 1000000000 * 10 ** decimals());
-        _setupRole(MINTER_ROLE, msg.sender);
-        _setupRole(UPGRADER_ROLE, msg.sender);
-    }
-
-    function snapshot() public onlyRole(SNAPSHOT_ROLE) {
-        _snapshot();
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(UPGRADER_ROLE, msg.sender);
     }
 
     function pause() public onlyRole(PAUSER_ROLE) {
@@ -59,7 +49,7 @@ contract StatefulArt is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeabl
     function _beforeTokenTransfer(address from, address to, uint256 amount)
         internal
         whenNotPaused
-        override(ERC20Upgradeable, ERC20SnapshotUpgradeable)
+        override
     {
         super._beforeTokenTransfer(from, to, amount);
     }
